7618952e760f851e1862bf16ccbec78e44011649
paul3042 <173724663+paul3042@users.noreply.github.com>
2025-06-11 22:10:03 +0100

    fin carousels

--- frontend/src/apiCalls/tmdbCalls.js
+++ frontend/src/apiCalls/tmdbCalls.js
@@ -11,6 +11,22 @@ const callFetchPopular = async () => {
     return data;
 };
 
+const callFetchUpcoming = async () => {
+    const response = await fetch('api/tmdb/upcoming', options);
+
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.message);
+    return data;
+};
+
+const callFetchNowPlaying = async () => {
+    const response = await fetch('api/tmdb/now-playing', options);
+
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.message);
+    return data;
+};
+
 const callFetchVideosById = async (id) => {
     const response = await fetch(`api/tmdb/${id}`, options);
 
@@ -19,4 +35,4 @@ const callFetchVideosById = async (id) => {
     return data;
 }
 
-export { callFetchPopular, callFetchVideosById };
+export { callFetchPopular, callFetchUpcoming, callFetchNowPlaying, callFetchVideosById };
