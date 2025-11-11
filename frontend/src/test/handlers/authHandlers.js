d8979d724fccbf63c2699b48a6c299b2b603972a
paul3042 <173724663+paul3042@users.noreply.github.com>
2025-11-11 22:18:57 +0000

    finish frontend auth tests

--- frontend/src/test/handlers/authHandlers.js
+++ frontend/src/test/handlers/authHandlers.js
@@ -1,13 +1,13 @@
 import { http, HttpResponse } from 'msw';
 
 export const authHandlers = [
-    http.get('/api/auth/authenticate-user', (req, rs) => {
+    http.post('/api/auth/register-user', () => {
         return HttpResponse.json(
             {
-                message: 'User authenticated.',
+                message: 'User created and logged in.',
                 user: {
                     id: 1,
-                    username: 'username',
+                    username: 'regUser',
                     created_at: '2025-01-01T00:00:00.000Z',
                     updated_at: '2025-01-01T00:00:00.000Z'
                 }
@@ -15,4 +15,39 @@ export const authHandlers = [
             { status: 200 }
         );
     }),
+
+    http.get('/api/auth/authenticate-user', () => {
+        return HttpResponse.json(
+            {
+                message: 'User authenticated.',
+                user: {
+                    id: 1,
+                    username: 'authUser',
+                    created_at: '2025-01-01T00:00:00.000Z',
+                    updated_at: '2025-01-01T00:00:00.000Z'
+                }
+            },
+            { status: 200 }
+        );
+    }),
+
+    http.post('/api/auth/login', () => {
+        return HttpResponse.json(
+            {
+                message: 'Log in succesful.',
+                user: {
+                    id: 1,
+                    username: 'loginUser',
+                    created_at: '2025-01-01T00:00:00.000Z',
+                    updated_at: '2025-01-01T00:00:00.000Z'
+                }
+            },
+            { status: 200 }
+        );
+    }),
+
+    http.post('/api/auth/logout', () => {
+        return HttpResponse.json({ status: 204 });
+    }),
 ];
+
