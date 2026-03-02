d0b1d26d7734b70ac296fe34ec16289d16bddff3
paul3042 <173724663+paul3042@users.noreply.github.com>
2026-03-02 22:17:59 +0000

    debug TMDB routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,13 +10,13 @@ import moviesRouter from './routes/moviesRoutes.js';
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(helmet());
 app.use(express.json());
 app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.set('trust proxy', 1);
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
